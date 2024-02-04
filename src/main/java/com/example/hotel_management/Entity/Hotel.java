08db6954b0b08d61be9089b125e0080697acf516
driss <jabri.dri.fst@uhp.ac.ma>
2024-02-04 09:13:07 +0100

    ;

--- src/main/java/com/example/hotel_management/Entity/Hotel.java
+++ src/main/java/com/example/hotel_management/Entity/Hotel.java
@@ -2,10 +2,7 @@ package com.example.hotel_management.Entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.sql.Blob;
 import java.util.List;
@@ -14,6 +11,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "HOTELS")
 public class Hotel {
